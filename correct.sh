9facce14389e78323cda3f60617fc314cdd9597b
Renat Nasridinov <rino@MacBook-Air.local>
2022-04-08 22:52:14 +0300

    Change sed command

--- correct.sh
+++ correct.sh
@@ -3,7 +3,9 @@
 for f in *.xml
 do 
 echo "Processing $f file.."
-# sed -i -e 's/&#([1-9]|[12][0-9]|3[01])//g' -e 's/&#127;\+//g' $f
-# sed -i 's/&quot;/\"/g' $f
-sed -i -e 's/&#30;\+//g' -e 's/&#3;\+//g' -e 's/&#127;\+//g' -e 's/&#24;\+//g' -e 's/&#14;\+//g' -e 's/&#31;\+//g' $f
+if [[ "$OSTYPE" == "linux"* ]]; then
+	LC_ALL=C sed -i -f sed_comm.txt -e $f
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	LC_ALL=C sed -i '' -f sed_comm.txt -r $f
+fi
 done
